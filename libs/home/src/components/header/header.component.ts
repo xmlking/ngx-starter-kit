8e06893d27ad80d3d1818f3ef19baab4f08ec57f
Sumanth Chinthagunta <xmlking+github@gmail.com>
2018-01-24 00:45:18 -0800

    ✨ 5. Theme Work-in-progress

--- libs/home/src/components/header/header.component.ts
+++ libs/home/src/components/header/header.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+const CDK = 'guides';
+const COMPONENTS = 'blog';
+
+export const SECTIONS = {
+  [COMPONENTS]: 'Blog',
+  [CDK]: 'Guides',
+};
+const SECTIONS_KEYS = Object.keys(SECTIONS);
+
 @Component({
   selector: 'app-home-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  get sections() {
+    return SECTIONS;
+  }
+
+  get sectionKeys() {
+    return SECTIONS_KEYS;
+  }
 }
