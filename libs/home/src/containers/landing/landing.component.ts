508f148e1b6fef8dca097a19b8d1e542f67ae143
Sumanth Chinthagunta <xmlking+github@gmail.com>
2018-01-24 00:46:30 -0800

    ✨ 5. Theme Work-in-progress

--- libs/home/src/containers/landing/landing.component.ts
+++ libs/home/src/containers/landing/landing.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
   constructor() {}
