8c132265c259c2c8d86b566373289e5012d4e2e6
Sumanth Chinthagunta <xmlking+github@gmail.com>
2018-04-06 11:11:43 -0700

    lib update

--- libs/auth/src/auth.actions.ts
+++ libs/auth/src/auth.actions.ts
@@ -6,23 +6,36 @@ export enum AuthMode {
 
 //  Actions
 export class Login {
+  static readonly type = '[Auth] Login';
   constructor(public readonly payload?: { infoMsg?: string }) {}
 }
-export class Logout {}
+export class Logout {
+  static readonly type = '[Auth] Logout';
+}
 export class LoadProfile {
+  static readonly type = '[Auth] Load Profile';
   constructor(public payload: any) {}
 }
 export class ChangeAuthMode {
+  static readonly type = '[Auth] Change Auth Mode';
   constructor(public readonly payload: AuthMode) {}
 }
 
 // Mutation Actions
 export class LoginSuccess {
+  static readonly type = '[Auth] Login Success';
   constructor(public readonly payload: any) {}
 }
-export class LoginCanceled {}
-export class LogoutSuccess {}
-export class ProfileLoaded {}
+export class LoginCanceled {
+  static readonly type = '[Auth] Login Canceled';
+}
+export class LogoutSuccess {
+  static readonly type = '[Auth] Logout Success';
+}
+export class ProfileLoaded {
+  static readonly type = '[Auth] Profile Loaded';
+}
 export class AuthModeChanged {
+  static readonly type = '[Auth] Auth Mode Changed';
   constructor(public readonly payload: AuthMode) {}
 }
