14d9297f3c2a7a750e4c02306fcd96e24d57d119
Sumanth Chinthagunta <xmlking+github@gmail.com>
2018-04-11 16:35:39 -0700

    actionHandler regression

--- apps/default/src/app/core/core.module.ts
+++ apps/default/src/app/core/core.module.ts
@@ -1,22 +1,25 @@
-import { NgModule, Optional, SkipSelf } from '@angular/core';
+import {APP_INITIALIZER, NgModule, Optional, SkipSelf} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './services/in-memory-data.service';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { NgxsModule } from '@ngxs/store';
+import { NgxPageScrollModule } from 'ngx-page-scroll';
+import { AuthModule, AuthState } from '@nx-starter-kit/auth';
+import { NavigatorModule, MenuState } from '@nx-starter-kit/navigator';
 import { environment } from '@env/environment';
 import { PageTitleService } from './services/page-title/page-title.service';
 import { ServiceWorkerService } from './services/service-worker/service-worker.service';
 import { MediaQueryService } from './services/mediareplay/media-replay.service';
-import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
-import { NgxPageScrollModule } from 'ngx-page-scroll';
 import { ErrorInterceptor } from './interceptors/error.interceptor';
 import { EventBus } from './state/eventbus';
 import { defaultMenu, demoMenu, adminMenu } from './menu-data';
-import { AuthModule, AuthState } from '@nx-starter-kit/auth';
-import { NavigatorModule, MenuState } from '@nx-starter-kit/navigator';
 import { RouterState } from './state/router.state';
 import { PreferenceState } from './state/preference.state';
+import { InMemoryDataService } from './services/in-memory-data.service';
+
+// Noop handler for factory function
+function noop() { return function() {}; }
 
 @NgModule({
   imports: [
@@ -47,12 +50,18 @@ import { PreferenceState } from './state/preference.state';
       provide: HTTP_INTERCEPTORS,
       useClass: ErrorInterceptor,
       multi: true
-    }
+    },
     // {
     //   provide: HTTP_INTERCEPTORS,
     //   useClass: JwtInterceptor,
     //   multi: true
-    // }
+    // },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: noop,
+      deps: [EventBus],
+      multi: true
+    }
   ]
 })
 export class CoreModule {
--- apps/default/src/app/core/state/eventbus.ts
+++ apps/default/src/app/core/state/eventbus.ts
@@ -1,10 +1,12 @@
-import { Actions, ofAction } from '@ngxs/store';
+import { Actions, ofAction, ofActionComplete, ofActionDispatched } from '@ngxs/store';
 import { Injectable } from '@angular/core';
 import { Login } from '@nx-starter-kit/auth';
 
 @Injectable()
 export class EventBus {
   constructor(private actions$: Actions) {
-    this.actions$.pipe(ofAction(Login)).subscribe(action => console.log('New Login!.......'));
+    this.actions$.pipe(ofActionDispatched(Login)).subscribe(action => console.log('Login.......Action Dispatched'));
+    this.actions$.pipe(ofActionComplete(Login)).subscribe(action => console.log('Login........Action Completed'));
+    // this.actions$.pipe(ofActionErrored(Login)).subscribe(action => console.log('Login........Action Errored'));
   }
 }
