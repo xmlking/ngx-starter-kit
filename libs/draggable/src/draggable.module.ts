8dd3ba8130100021e8d64e0f3ea252667a3f7283
Sumanth Chinthagunta <xmlking+github@gmail.com>
2018-04-15 13:30:14 -0700

    draggable - resizeable

---
+++ libs/draggable/src/draggable.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {DraggableDirective} from "./directives/draggable/draggable.directive";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [DraggableDirective],
+  exports: [DraggableDirective]
+})
+export class DraggableModule {}
