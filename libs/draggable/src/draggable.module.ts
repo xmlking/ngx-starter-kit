81fe3745b1351f4bf4b8d5db8e0d391ffb9e0052
xmlking <xmlking@gmail.com>
2018-04-22 17:16:30 -0700

    resizable

--- libs/draggable/src/draggable.module.ts
+++ libs/draggable/src/draggable.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DraggableDirective } from './directives/draggable/draggable.directive';
+import { ResizableComponent } from './directives/resizable/resizable.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [DraggableDirective],
-  exports: [DraggableDirective]
+  declarations: [DraggableDirective, ResizableComponent],
+  exports: [DraggableDirective, ResizableComponent]
 })
 export class DraggableModule {}
