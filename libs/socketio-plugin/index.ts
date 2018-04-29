457bc50cb166bad4c3ddc07634f0af311013b5f3
xmlking <xmlking@gmail.com>
2018-04-29 05:10:02 -0700

    polish

--- libs/socketio-plugin/index.ts
+++ libs/socketio-plugin/index.ts
@@ -1,1 +1,2 @@
 export { NgxsSocketioPluginModule } from './src/socketio-plugin.module';
+export { NgxsSocketioPluginOptions, ConnectSocketio, SocketioMessageError, DisconnectSocketio, SendSocketioMessage } from './src/symbols';
