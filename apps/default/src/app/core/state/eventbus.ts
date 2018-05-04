59b44e9debd86772fba9d489dadb41b810fb0eb7
xmlking <xmlking@gmail.com>
2018-05-04 01:13:30 -0700

    lib update

--- apps/default/src/app/core/state/eventbus.ts
+++ apps/default/src/app/core/state/eventbus.ts
@@ -1,4 +1,4 @@
-import { Actions, ofActionCompleted, ofActionErrored, ofActionDispatched } from '@ngxs/store';
+import { Actions, ofActionSuccessful, ofActionErrored, ofActionDispatched } from '@ngxs/store';
 import { Injectable } from '@angular/core';
 import { Login } from '@nx-starter-kit/auth';
 
@@ -6,7 +6,7 @@ import { Login } from '@nx-starter-kit/auth';
 export class EventBus {
   constructor(private actions$: Actions) {
     this.actions$.pipe(ofActionDispatched(Login)).subscribe(action => console.log('Login.......Action Dispatched'));
-    this.actions$.pipe(ofActionCompleted(Login)).subscribe(action => console.log('Login........Action Completed'));
+    this.actions$.pipe(ofActionSuccessful(Login)).subscribe(action => console.log('Login........Action Successful'));
     this.actions$.pipe(ofActionErrored(Login)).subscribe(action => console.log('Login........Action Errored'));
   }
 }
