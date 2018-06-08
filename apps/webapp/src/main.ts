a94f898b83bc4b706826ffb40c3aa0a1ecc8511f
xmlking <xmlking@gmail.com>
2018-06-08 14:23:37 -0700

    virtual-scroll example

--- apps/webapp/src/main.ts
+++ apps/webapp/src/main.ts
@@ -1,11 +1,13 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { setAutoFreeze } from "immer";
 
 import { AppModule } from './app/app.module';
 import { environment } from '@env/environment';
 
 if (environment.production) {
   enableProdMode();
+  // setAutoFreeze(false)
 }
 
 platformBrowserDynamic()
