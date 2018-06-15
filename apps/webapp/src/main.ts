f63cec7bef8adc10f5f9c55cde352d5e8ca4b623
xmlking <xmlking@gmail.com>
2018-06-15 14:53:15 -0700

    regression:  nx 6.1.0 upgrade

--- apps/webapp/src/main.ts
+++ apps/webapp/src/main.ts
@@ -1,13 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-// import { setAutoFreeze } from "immer";
 
 import { AppModule } from './app/app.module';
 import { environment } from '@env/environment';
 
 if (environment.production) {
   enableProdMode();
-  // setAutoFreeze(false)
 }
 
 platformBrowserDynamic()
