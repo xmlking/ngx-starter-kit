efba839eed441f220c186603bd72745fc21d6eec
xmlking <xmlking@gmail.com>
2018-06-21 09:14:51 -0700

    angular -> 6.1.0 removed keysPipe as we have native keyvalue pipe

--- libs/ngx-pipes/src/lib/ngx-pipes.module.ts
+++ libs/ngx-pipes/src/lib/ngx-pipes.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharactersPipe } from './truncate/characters.pipe';
 import { WordsPipe } from './truncate/words.pipe';
-import { KeysPipe } from './helper/keys.pipe';
 import { SafeHtmlPipe } from './helper/saft-html.pipe';
 import { FilterPipe } from './helper/filter.pipe';
 import { GroupByPipe } from './helper/group-by.pipe';
 
-export const PIPES = [CharactersPipe, WordsPipe, KeysPipe, SafeHtmlPipe, FilterPipe, GroupByPipe];
+export const PIPES = [CharactersPipe, WordsPipe, SafeHtmlPipe, FilterPipe, GroupByPipe];
 
 @NgModule({
   imports: [CommonModule],
