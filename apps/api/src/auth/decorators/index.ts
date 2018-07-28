96063e9c8dd43cb959162ab91002b26ba413092f
xmlking <xmlking@gmail.com>
2018-07-27 17:25:00 -0700

    added raw-token decorator

--- apps/api/src/auth/decorators/index.ts
+++ apps/api/src/auth/decorators/index.ts
@@ -3,3 +3,4 @@ export * from './allow.decorator';
 export * from './claims.decorator';
 export * from './current-user.decorator';
 export * from './token.decorator';
+export * from './raw-token.decorator';
