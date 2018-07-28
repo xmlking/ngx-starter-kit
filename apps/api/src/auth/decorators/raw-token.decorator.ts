fc2ed82cb5901266de6074d1f3c317dd11a66958
xmlking <xmlking@gmail.com>
2018-07-27 17:25:00 -0700

    added raw-token decorator

---
+++ apps/api/src/auth/decorators/raw-token.decorator.ts
@@ -0,0 +1,7 @@
+import { createParamDecorator } from '@nestjs/common';
+import {ExtractJwt} from 'passport-jwt';
+const extractor = ExtractJwt.fromAuthHeaderAsBearerToken();
+
+export const RawToken = createParamDecorator((data, req) => {
+  return extractor(req);
+});
