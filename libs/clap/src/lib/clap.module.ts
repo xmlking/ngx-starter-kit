56245eb7968fa8aace4d3af5956f127c0ebc94bf
xmlking <xmlking@gmail.com>
2018-08-12 15:57:03 -0700

    new: Adding Clap Micro Interaction

---
+++ libs/clap/src/lib/clap.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClapComponent } from './clap.component';
+import { CounterBubbleComponent } from './components/counter-bubble.component';
+import { TotalCounterComponent } from './components/total-counter.component';
+import { FabComponent } from './components/fab.component';
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ClapComponent, CounterBubbleComponent, TotalCounterComponent, FabComponent],
+  exports: [ClapComponent]
+})
+export class ClapModule {}
---
+++ libs/clap/src/lib/components/fab.component.ts
@@ -0,0 +1,51 @@
+import { transition, trigger, useAnimation } from '@angular/animations';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Input
+} from '@angular/core';
+import { pulseAnimation } from '../animations';
+
+@Component({
+  selector: 'ngx-fab',
+  template: `
+    <i class="material-icons">pan_tool</i>
+  `,
+  styles: [
+    `
+      :host {
+        border: 1px solid rgba(0, 0, 0, 0.7);
+        color: #02b875;
+        width: 64px;
+        height: 64px;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        user-select: none;
+        box-shadow: 2px 2px 7px 0px rgba(0, 0, 0, 0.6);
+      }
+    `
+  ],
+  animations: [
+    trigger('counterChange', [
+      transition(
+        ':increment',
+        useAnimation(pulseAnimation, {
+          params: {
+            timings: '400ms cubic-bezier(.11,.99,.83,.43)',
+            scale: 1.05
+          }
+        })
+      )
+    ])
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FabComponent {
+  @HostBinding('@counterChange')
+  @Input()
+  counter: number;
+}
