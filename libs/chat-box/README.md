ae2a26b6acbd9187169bb20fe2033e7a61caffb9
xmlking <xmlking@gmail.com>
2018-08-16 13:52:33 -0700

    knob demo

---
+++ libs/chat-box/README.md
@@ -0,0 +1,13 @@
+Chat-Box
+========
+
+### Goals
+* hybrid conversations with bots, users and groups.
+* multiple active conversations
+* save and retrieve active conversations
+
+
+
+TODO
+multiple conversations inspired by 
+https://www.weave.works/
---
+++ libs/experiments/src/lib/containers/knob-demo/knob-demo.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { KnobDemoComponent } from './knob-demo.component';
+
+describe('KnobDemoComponent', () => {
+  let component: KnobDemoComponent;
+  let fixture: ComponentFixture<KnobDemoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ KnobDemoComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(KnobDemoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
