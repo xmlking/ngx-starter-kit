cc6d3fc65f9acd3cba532c4b88a2b8c6aca21272
xmlking <xmlking@gmail.com>
2018-08-31 10:14:53 -0700

    added DeepPartial

---
+++ libs/utils/src/lib/DeepPartial.ts
@@ -0,0 +1,6 @@
+/**
+ * Same as Partial<T> but goes deeper and makes Partial<T> all its properties and sub-properties.
+ */
+export type DeepPartial<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>;
+};
