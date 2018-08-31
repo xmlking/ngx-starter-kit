fb323058f76a0d5abfde0e19970b8331066cc47a
xmlking <xmlking@gmail.com>
2018-08-31 10:14:53 -0700

    added DeepPartial

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,2 +1,4 @@
 export { StateDef } from './lib/StateDef';
 export { waitUntil } from './lib/utils';
+export { DeepPartial } from './lib/DeepPartial';
+export { ObjectLiteral } from './lib/ObjectLiteral';
