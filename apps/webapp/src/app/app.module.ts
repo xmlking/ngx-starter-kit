3591b4c674c9988badc2e32c91bfbb81ae61614f
xmlking <xmlking@gmail.com>
2018-09-04 02:27:04 -0700

    added Formly

--- apps/webapp/src/app/app.module.ts
+++ apps/webapp/src/app/app.module.ts
@@ -1,20 +1,21 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import * as Hammer from 'hammerjs';
 import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 import { PreloadAllModules, RouterModule } from '@angular/router';
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from '@ngx-starter-kit/core';
 
-import {environment} from '@env/environment';
+import { environment } from '@env/environment';
+import { PreloadSelectedModulesList } from './preloading-strategy';
 
 export class MyHammerConfig extends HammerGestureConfig {
   overrides = <any>{
     // override hammerjs default configuration
-    pan: {direction: Hammer.DIRECTION_ALL },
-    swipe: { direction: Hammer.DIRECTION_ALL }
+    pan: { direction: Hammer.DIRECTION_ALL },
+    swipe: { direction: Hammer.DIRECTION_ALL },
   };
 }
 
@@ -24,33 +25,33 @@ export class MyHammerConfig extends HammerGestureConfig {
     BrowserAnimationsModule,
     RouterModule.forRoot(
       [
-        {path: '', redirectTo: 'home', pathMatch: 'full'},
-        {path: 'home', loadChildren: '@ngx-starter-kit/home#HomeModule'},
-        {path: 'dashboard', loadChildren: '@ngx-starter-kit/dashboard#DashboardModule'},
-        {path: '404', loadChildren: '@ngx-starter-kit/not-found#NotFoundModule'},
+        { path: '', redirectTo: 'home', pathMatch: 'full' },
+        { path: 'home', loadChildren: '@ngx-starter-kit/home#HomeModule', data: { preload: true } },
+        { path: 'dashboard', loadChildren: '@ngx-starter-kit/dashboard#DashboardModule', data: { preload: true } },
+        { path: '404', loadChildren: '@ngx-starter-kit/not-found#NotFoundModule' },
         // 404 should be last
-        {path: '**', redirectTo: '404', pathMatch: 'full'}
+        { path: '**', redirectTo: '404', pathMatch: 'full' },
       ],
       {
         scrollPositionRestoration: 'enabled',
         anchorScrolling: 'enabled',
         initialNavigation: 'enabled',
-        preloadingStrategy: PreloadAllModules,
-        paramsInheritanceStrategy: 'always'
+        preloadingStrategy: PreloadAllModules, // TODO: PreloadSelectedModulesList
+        paramsInheritanceStrategy: 'always',
         // onSameUrlNavigation: 'reload'
-      }
+      },
     ),
-    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     CoreModule, // IMP: Please keep CoreModule after RouterModule
   ],
   providers: [
+    PreloadSelectedModulesList,
     {
       provide: HAMMER_GESTURE_CONFIG,
-      useClass: MyHammerConfig
-    }
+      useClass: MyHammerConfig,
+    },
   ],
   declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
