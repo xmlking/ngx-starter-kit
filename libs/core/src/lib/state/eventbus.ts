600837e53e410d907013dc181c9a80cb504b1643
xmlking <xmlking@gmail.com>
2018-11-18 09:25:38 -0800

    fix(eventbus): workaround for ngxs router plugin issue
    https://github.com/ngxs/store/issues/542

--- libs/core/src/lib/state/eventbus.ts
+++ libs/core/src/lib/state/eventbus.ts
@@ -8,11 +8,12 @@ import {
   WebSocketConnected,
   WebSocketDisconnected,
 } from '@ngx-starter-kit/socketio-plugin';
-import { RouterNavigation } from '@ngxs/router-plugin';
+import { RouterNavigation, RouterState } from '@ngxs/router-plugin';
 import { RouterStateData } from '@ngx-starter-kit/core';
-import { distinctUntilChanged, map } from 'rxjs/operators';
+import { distinctUntilChanged, filter, map } from 'rxjs/operators';
 import { PageTitleService } from '../services/page-title.service';
 import { GoogleAnalyticsService } from '../services/google-analytics.service';
+import { NavigationEnd, Router, RouterEvent } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -21,6 +22,7 @@ export class EventBus {
   constructor(
     private actions$: Actions,
     private store: Store,
+    private router: Router,
     private analytics: GoogleAnalyticsService,
     private pageTitle: PageTitleService,
   ) {
@@ -47,17 +49,32 @@ export class EventBus {
       .subscribe(action => console.log('WebSocketDisconnected........Action Successful'));
 
     // FIXME : https://github.com/ngxs/store/issues/542
-    this.actions$
+    // this.actions$
+    //   .pipe(
+    //     ofActionSuccessful(RouterNavigation),
+    //     map((action: RouterNavigation) => action.routerState as any),
+    //     distinctUntilChanged((previous: RouterStateData, current: RouterStateData) => {
+    //       return previous.url === current.url;
+    //     }),
+    //   )
+    //   .subscribe(data => {
+    //     this.pageTitle.setTitle(data.breadcrumbs);
+    //     this.analytics.setPage(data.url);
+    //   });
+
+    // WORKAROUND
+    this.router.events
       .pipe(
-        ofActionSuccessful(RouterNavigation),
-        map((action: RouterNavigation) => action.routerState as any),
-        distinctUntilChanged((previous: RouterStateData, current: RouterStateData) => {
+        filter(event => event instanceof NavigationEnd),
+        distinctUntilChanged((previous: any, current: RouterEvent) => {
           return previous.url === current.url;
         }),
       )
-      .subscribe(data => {
+      .subscribe((event: NavigationEnd) => {
+        const data = this.store.selectSnapshot<RouterStateData>(RouterState.state);
         this.pageTitle.setTitle(data.breadcrumbs);
         this.analytics.setPage(data.url);
       });
+
   }
 }
