46abb0faadd9addc5238f555781cf1e652211e85
xmlking <xmlking@gmail.com>
2018-12-08 15:56:09 -0800

    feat(entity): added column templates and sticky column options

--- libs/shared/src/lib/containers/entity/entity.model.ts
+++ libs/shared/src/lib/containers/entity/entity.model.ts
@@ -1,3 +1,5 @@
+import { TemplateRef } from '@angular/core';
+
 export abstract class Entity {
   abstract get id(): number | string;
   [key: string]: any;
@@ -10,6 +12,8 @@ export class EntityColumnDef<T> {
   readonly property: string;
   readonly header = this.property;
   public visible = true;
+  readonly sticky?: 'start' | 'end';
+  readonly template?: TemplateRef<any>;
   readonly displayFn = (entity: T) => entity[this.property];
 
   public constructor(init?: Partial<EntityColumnDef<T>>) {
