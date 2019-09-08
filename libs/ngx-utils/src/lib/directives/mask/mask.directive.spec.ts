f91fdcf5de24ab104a4f43d6007b4c9595d0a517
Sumanth Chinthagunta <xmlking@gmail.com>
2019-09-07 22:16:05 -0700

    fix(actions): fix github actions workflow

--- libs/ngx-utils/src/lib/directives/mask/mask.directive.spec.ts
+++ libs/ngx-utils/src/lib/directives/mask/mask.directive.spec.ts
@@ -1,8 +1,14 @@
+import { ElementRef } from '@angular/core';
 import { MaskDirective } from './mask.directive';
+export class MockElementRef extends ElementRef {
+  constructor() {
+    super(null);
+  }
+}
 
 describe('MaskDirective', () => {
   it('should create an instance', () => {
-    const directive = new MaskDirective();
+    const directive = new MaskDirective(new MockElementRef());
     expect(directive).toBeTruthy();
   });
 });
