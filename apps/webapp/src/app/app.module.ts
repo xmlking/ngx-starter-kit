9e92362c37aae533c5e9c053b18d9c94245519b0
Sumanth Chinthagunta <xmlking@gmail.com>
2020-03-26 20:22:17 -0700

    feat(angular): upgraded angular to 9.1.0

--- apps/webapp/src/app/app.module.ts
+++ apps/webapp/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '@env/environment';
 import { CoreModule } from '@ngx-starter-kit/core';
-import { QuicklinkModule, QuicklinkStrategy } from '@xmlking/ngx-quicklink';
+import { QuicklinkModule, QuicklinkStrategy } from 'ngx-quicklink';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -18,40 +18,40 @@ import { AppComponent } from './app.component';
         { path: '', redirectTo: 'home', pathMatch: 'full' },
         {
           path: 'home',
-          loadChildren: () => import('@ngx-starter-kit/home').then(module => module.HomeModule),
-          data: { preload: true }
+          loadChildren: () => import('@ngx-starter-kit/home').then((module) => module.HomeModule),
+          data: { preload: true },
         },
         {
           path: 'dashboard',
-          loadChildren: () => import('@ngx-starter-kit/dashboard').then(module => module.DashboardModule),
-          data: { preload: true }
+          loadChildren: () => import('@ngx-starter-kit/dashboard').then((module) => module.DashboardModule),
+          data: { preload: true },
         },
         {
           path: 'admin',
-          loadChildren: () => import('@ngx-starter-kit/admin').then(module => module.AdminModule),
-          data: { preload: false }
+          loadChildren: () => import('@ngx-starter-kit/admin').then((module) => module.AdminModule),
+          data: { preload: false },
         },
         {
           path: '404',
-          loadChildren: () => import('@ngx-starter-kit/not-found').then(module => module.NotFoundModule),
-          data: { title: '404', preload: false }
+          loadChildren: () => import('@ngx-starter-kit/not-found').then((module) => module.NotFoundModule),
+          data: { title: '404', preload: false },
         },
         // 404 should be last
-        { path: '**', redirectTo: '404', pathMatch: 'full' }
+        { path: '**', redirectTo: '404', pathMatch: 'full' },
       ],
       {
         scrollPositionRestoration: 'enabled',
         anchorScrolling: 'enabled',
         preloadingStrategy: QuicklinkStrategy,
-        paramsInheritanceStrategy: 'always'
+        paramsInheritanceStrategy: 'always',
         // enableTracing: true, // enable to debug routing during development
         // onSameUrlNavigation: 'reload'
       }
     ),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    CoreModule // IMP: Please keep CoreModule after RouterModule
+    CoreModule, // IMP: Please keep CoreModule after RouterModule
   ],
   declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
